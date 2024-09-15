f6c4d0890bc06d54ccf5933b08a2f59f87b5be8d
starlabman <akodjolabore@gmail.com>
2024-09-15 11:30:07 +0000

    update code

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const web3 = new Web3("https://1rpc.io/sepolia");
 async function deploy() {
 
     // Initialize a Wallet with a private key
-    const wallet = web3.eth.wallet.add("0x50d349f5cf627d44858d6fcb6fbf15d27457d35c58ba2d5cfeaf455f25db5bec");
+    const wallet = web3.eth.wallet.add("0xaa140eef69e2c04f5bd0773e97238ae1c009cd09e03c5e503bfc86ed80011ec6");
     
     // console.log(wallet);
     // console.log(wallet[0].address);
@@ -21,7 +21,7 @@ async function deploy() {
 
 
 
-    // initialize contract aa140eef69e2c04f5bd0773e97238ae1c009cd09e03c5e503bfc86ed80011ec6
+    // initialize contract 
 
      const myContract = new web3.eth.Contract(ABI);
 
